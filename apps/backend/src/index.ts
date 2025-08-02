863de8c40d0bd1b7f68aac72ae0d02073124233d
harshkhosla <harsh.khosla@netomi.com>
2025-08-02 13:54:52 +0530

    adding the correct for the node js application and creatintga react application

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import v1Router from "./routes/v1";
+import v1Router from "./routes/v1/index";
 import cors from "cors";
 
 const app = express();
+const PORT = 4000
 
 app.use(cors());
 app.use(express.json());
 
 app.use("api/v1", v1Router);
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log("HTTP Backend Working",PORT);
+})
