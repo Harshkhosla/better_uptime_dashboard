d6f4ee782dff535c3b3392faed85e5818ad484e5
harshkhosla <harsh.khosla@netomi.com>
2025-08-04 15:30:28 +0530

    adding the the worker and consumer

---
+++ apps/producer/eslint.config.js
@@ -0,0 +1,4 @@
+// packages/common/eslint.config.js
+import { config } from "@repo/eslint-config/base";
+
+export default config;
---
+++ apps/worker/eslint.config.js
@@ -0,0 +1,5 @@
+
+// packages/common/eslint.config.js
+import { config } from "@repo/eslint-config/base";
+
+export default config;
