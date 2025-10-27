ee70c541b6928548b4b50722876dd0a78e2e426a
harshkhosla <harsh.khosla@netomi.com>
2025-10-27 19:10:38 +0530

    correcting it

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,3 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+// @ts-ignore - Prisma generated client
+import { PrismaClient } from "../generated/prisma";
 
 export const Prismaclient = new PrismaClient();
