2ee73f7818444412146fb991b299df40ce609e48
harshkhosla <harsh.khosla@netomi.com>
2025-11-26 12:10:31 +0530

    correctin the project initlization

--- apps/fitness-web/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Layout Components
-export { default as Navigation } from "./layout/Navigation";
-export { default as Footer } from "./layout/Footer";
-
-// Section Components
-export { default as HeroSection } from "./sections/HeroSection";
-export { default as FeaturesSection } from "./sections/FeaturesSection";
-export { default as HowItWorksSection } from "./sections/HowItWorksSection";
-export { default as CTASection } from "./sections/CTASection";
--- apps/fitness-web/src/pages/HowItWorksPage.tsx
+++
@@ -1,9 +0,0 @@
-import HowItWorksSection from "../components/sections/HowItWorksSection";
-
-export default function HowItWorksPage() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50/50 via-white to-purple-50/30">
-      <HowItWorksSection />
-    </div>
-  );
-}
